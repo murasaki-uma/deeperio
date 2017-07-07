b821934124b617c2f3beba62c68b506560bcaa10
uma_murasaki <uma9266@gmail.com>
2017-07-08 04:53:12 +0900

    add models

--- src/js/Scene01.ts
+++ src/js/Scene01.ts
@@ -1,4 +1,6 @@
-
+import "./loaders/MTLLoader.js";
+import "./loaders/DDSLoader.js";
+import "./loaders/OBJLoader.js";
 // *********** ひとつめのシーン *********** //
 export default class SceneTemplate{
 
@@ -23,20 +25,28 @@ export default class SceneTemplate{
 
         this.scene = new THREE.Scene();
 
-        // 立方体のジオメトリーを作成
-        this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        // 緑のマテリアルを作成
-        this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        // 上記作成のジオメトリーとマテリアルを合わせてメッシュを生成
-        this.cube = new THREE.Mesh( this.geometry, this.material );
-        // メッシュをシーンに追加
-        this.scene.add( this.cube );
+        // // 立方体のジオメトリーを作成
+        // this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        // // 緑のマテリアルを作成
+        // this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        // // 上記作成のジオメトリーとマテリアルを合わせてメッシュを生成
+        // this.cube = new THREE.Mesh( this.geometry, this.material );
+        // // メッシュをシーンに追加
+        // this.scene.add( this.cube );
+
+        let ambient = new THREE.AmbientLight(0xffffff);
+        this.scene.add(ambient);
+
+        THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader() );
+        let mtlLoader = new THREE.MTLLoader();
+        mtlLoader.setPath( 'models/pal/' );
+
+
 
         // カメラを作成
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
         // カメラ位置を設定
         this.camera.position.z = 5;
-
 
 
     }
@@ -55,6 +65,13 @@ export default class SceneTemplate{
     }
 
     // ******************************************************
+    public keyDown(e:KeyboardEvent)
+    {
+
+    }
+
+
+    // ******************************************************
     public mouseMove(e:MouseEvent)
     {
 
@@ -71,8 +88,8 @@ export default class SceneTemplate{
     public update(time)
     {
 
-        this.cube.rotation.x += 0.1;
-        this.cube.rotation.y += 0.1;
+        // this.cube.rotation.x += 0.1;
+        // this.cube.rotation.y += 0.1;
 
     }
 
