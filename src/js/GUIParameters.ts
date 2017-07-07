9fedba8001564465c9492c6a128e020c37b4f109
YumaMurasaki <uma9266@gmail.com>
2017-07-08 02:29:56 +0900

    base file

---
+++ src/js/GUIParameters.ts
@@ -0,0 +1,30 @@
+export default class GUIParameters
+{
+
+    public fadeInSpeed:number = 1.3;
+    public particleSpeed:number = 0.3;
+    public radiusSpeed:number = 0.1;
+    public particleMaxRad:number = 3000;
+    public particleMinRad:number = 1000;
+    public particleScaleUpSpeed:number = 0.8;
+    public particleScaleDownSpeed:number = 0.1;
+    public randomRange_radius:number = 0.63;
+    public randomRange_speed:number = 0.5;
+
+
+
+    public isCameraUpdate:boolean = true;
+    public cameraSpeed:number= 0.01;
+    public cameraRotateX:number= 0.5;
+    public cameraRotateY:number= 0.3;
+    public cameraRotateZ:number= 0.4;
+    public delay_dropin:number = 0.0;
+    public delay_rotate:number = 1.0;
+    public position_z:number = 20.0;
+    public duration_dropin:number = 3.0;
+    public duration_rotate:number = 2.0;
+
+
+
+    // public borderRadiusSpeed:number = 0.01;
+};
---
+++ src/js/main.ts
@@ -0,0 +1,78 @@
+"use strict"
+declare function require(x: string): any;
+import * as $ from "jquery";
+import Template from "./template";
+import Scene01 from "./Scene01";
+import VThree from "./VThree";
+// const io = require('socket.io');
+
+class Main
+{
+    public vthree:VThree;
+    public test:Template;
+    public scene01:Scene01;
+    public socket:any;
+
+
+
+    constructor(num:number) {
+
+
+
+
+        // URLのアンカー（#以降の部分）を取得
+
+        $.getJSON("json/vthree.config.json" , (config) => {
+            $.getJSON("json/guisetting.json" , (data) => {
+                this.vthree = new VThree(1.0, false,config);
+                this.scene01 = new Scene01(this.vthree.renderer);
+                this.vthree.addScene(this.scene01);
+                this.vthree.draw();
+
+                this.vthree.isUpdate = true;
+
+                // this.socket = io.connect(); // C02. ソケットへの接続
+                // // this.socket.emit("client_to_server", {value : time});
+                // // C04. server_to_clientイベント・データを受信する
+                // this.socket.on("server_to_client", (data)=>{this.log(data.value)});
+                //
+                //
+                // // this.socket.on("psx", (data)=>{this.log(data.value)});
+                //
+                // // this.update();
+            });
+        });
+    }
+
+
+
+    public  update(time) {
+        requestAnimationFrame(this.update.bind(this));
+    }
+
+}
+
+
+window.onload = function() {
+//     var urlHash = location.hash;
+//
+// // URLにアンカーが存在する場合
+//     if(urlHash){
+//         // アンカーが#osakaかどうかを判断する
+//         console.log(urlHash.replace('#',''));
+//         const num = Number(urlHash.replace('#',''));
+//         const main = new Main(num);
+//     }else {
+//         location.href = location+'#300';
+//     }
+//
+//     $(".particleNum a").click(function () {
+//         location.reload();
+//         console.log('click');
+//     });
+
+
+
+    const main = new Main();
+
+}
